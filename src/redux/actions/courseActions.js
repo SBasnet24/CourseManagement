ac5032e4287efd46a4bf635416d08f03b4c616cf
Sujan Basnet <sujanbasnyet334@gmail.com>
2020-06-06 11:15:23 +0545

    a simple redux boilerplate

--- src/redux/actions/courseActions.js
+++ src/redux/actions/courseActions.js
@@ -1,9 +1,10 @@
 // simple action for creating a course
+import { CREATE_COURSE } from "./actionTypes";
 // corresponding to the context dispatch({type:,payload:})
 
 export const createCourse = (course) => {
   return {
-    type: "CREATE_COURSE",
+    type: CREATE_COURSE,
     course,
   };
 };
--- src/redux/reducers/courseReducer.js
+++ src/redux/reducers/courseReducer.js
@@ -1,6 +1,7 @@
+import { CREATE_COURSE } from "./../actions/actionTypes";
 const courses = (state = [], action) => {
   switch (action.type) {
-    case "CREATE_COURSE":
+    case CREATE_COURSE:
       return [...state, { ...action.course }];
     default:
       return state;
